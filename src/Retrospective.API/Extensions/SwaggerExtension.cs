932a69f112f25d220a38ce115ea85d950ea3b111
Vikas Sharma <mailbox.viksharma@gmail.com>
2020-07-12 12:50:07 +0530

    Swagger UI added

---
+++ src/Retrospective.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Retrospective.API.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Retrospective API",
+                    Description = "A project ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vikas Sharma",
+                        Email = "mailbox.viksharma@gmail.com",
+                        Url = new Uri("https://twitter.com/vikas5harma"),
+                    }
+                });
+            });
+        }
+
+        public static void AddSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Retrospective API V1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
--- src/Retrospective.API/Startup.cs
+++ src/Retrospective.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Retrospective.API.Extensions;
 using Retrospective.Application;
 using Retrospective.Infrastructure;
 
@@ -23,6 +24,8 @@ namespace Retrospective.API
             services.AddControllers();
             services.AddApplication(Configuration);
             services.AddInfrastructure(Configuration);
+
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +37,8 @@ namespace Retrospective.API
             }
 
             app.UseHttpsRedirection();
+
+            app.AddSwagger();
 
             app.UseRouting();
 
